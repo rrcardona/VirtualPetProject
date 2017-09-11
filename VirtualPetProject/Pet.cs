dd77c77d58d0d4c28caf0747a709c9a263c29233
rrcardona <cardonas05@yahoo.com>
2017-09-11 16:51:40 -0400

    Virtual Pet project

---
+++ VirtualPetProject/Pet.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPetProject
+{
+    class Pet
+    {
+        
+        //spike the alligator
+
+        private string name;
+        private int staminaTime;
+        private int huntingTime;
+        private int eatingTime;
+        private int bathroomTime;
+        private int sleepTime;
+        private int maxPower;
+
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+        public int Stamina
+        {
+            get { return this.staminaTime; }
+            
+        }
+        public int Survival
+        {
+            get { return this.huntingTime; }
+            
+        }
+        public int Eating
+        {
+            get { return this.eatingTime; }
+            
+        }
+        public int Waste
+        {
+            get { return this.bathroomTime; }
+        }
+           
+        public int Sleep
+        {
+            get { return this.sleepTime; }
+            
+        }
+        public int MaxPower
+        {
+            get { return this.maxPower; }
+        }
+        public Pet()
+        {
+            this.name = "Spike";
+            this.staminaTime = 5;
+            this.huntingTime = 5;
+            this.eatingTime = 0;
+            this.sleepTime = 0;
+            this.bathroomTime = 0;
+            this.maxPower = 5;                           
+        }
+        public Pet(string name)
+        {
+            this.name = name;
+        }
+        public Pet(string name, int eatingTime, int staminaTime, int huntingTime, int sleepTime, int bathroomTime)
+        {
+            this.name = name;
+            this.eatingTime = eatingTime;
+            this.staminaTime = staminaTime;
+            this.huntingTime = huntingTime;
+            this.sleepTime = sleepTime;
+            this.bathroomTime = bathroomTime;
+        }
+        public void Greeting()
+        {
+            Console.WriteLine("Hi, my name is Spike!");
+        }
+        public void PlayingTime ()
+        {
+            if(this.staminaTime<5)
+            {
+                Console.WriteLine("I'm tired. I need some rest");
+            } 
+            else
+            {
+                this.eatingTime += 1;
+                this.sleepTime += 1;
+                Console.WriteLine("That was fun!!! My current Stamina to play is {0}",this.staminaTime);
+            }
+        }
+        public void HuntingTime()
+        {
+            if (this.staminaTime <2)
+            {
+                Console.WriteLine("I'm too tired for a hunt. I need some sleep!");
+            }
+            else
+            {
+                this.bathroomTime = 1;
+                this.eatingTime = 2;
+                Console.WriteLine("That was fun. I can't wait to see whats on the menu!!!");
+            }
+        }
+        public void EatingTime()
+        {
+            if (eatingTime <4)
+            {
+                Console.WriteLine("I'm full!!!");
+            }
+            else
+            {
+                this.bathroomTime += 2;
+                this.sleepTime += 1;
+                Console.WriteLine("Whats on the menu? I'm ready for a feast!");
+            }            
+        }
+        public void BathroomTime()
+        {
+            if (bathroomTime < 5)
+            {
+                Console.WriteLine("I'm fine. Let's go do something fun");
+            }
+            else
+            {
+                this.eatingTime = 1;
+                this.staminaTime = 2;
+                Console.WriteLine("A little privacy please");
+            }
+        }
+        public void SleepTime()
+        {
+            Console.WriteLine("That was a great nap!!! I feel amazing");
+            this.staminaTime = maxPower;
+            this.huntingTime = maxPower;
+                    
+        }
+    }
+}
--- VirtualPetProject/Program.cs
+++ VirtualPetProject/Program.cs
@@ -8,8 +8,55 @@ namespace VirtualPetProject
 {
     class Program
     {
+        public static int EatingTime { get; private set; }
+        public static int SleepTime { get; private set; }
+
         static void Main(string[] args)
         {
-        }
+
+            Pet Spike = new Pet();
+            Console.WriteLine("Hi I'm Spike The Gada. I heard you were taking care of me for a while. Sounds fun!!!! Lets go to it gator");
+            do
+            {
+                Console.WriteLine("Hey gator, what do you wanna do now?\nPress 1 to Hunt \nPress 2 to Eat \nPress 3 to go to the Bathroom \nPress 4 to go to Sleep");
+                string input = Console.ReadLine();
+                switch (input.ToLower())
+                {
+                    case "1":
+                        Spike.HuntingTime();
+                        break;
+                    case "2":
+                        Spike.EatingTime();
+                        break;
+                    case "3":
+                        Spike.BathroomTime();
+                        break;
+                    case "4":
+                        Spike.SleepTime();
+                        break;
+                }
+            }
+            while (SleepTime >= 5 && EatingTime >= 5);          
+            {
+                Console.WriteLine("Thank you for taking care of me");
+            }
+
+
+            
+            
     }
+}
+
+ 
+
+
+
+
+
+
+
+
+
+
+
 }
